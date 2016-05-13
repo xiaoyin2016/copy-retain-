a7655b1b5895017d4df9a913020f4c9487dcd05d
mac <mac@macs-macbook-pro-2.local>
2016-05-13 19:59:11 +0800

    Initial Commit

---
+++ copy和retain的区别/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  copy和retain的区别
+//
+//  Created by mac on 16/5/13.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
